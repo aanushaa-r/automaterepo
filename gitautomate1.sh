0ab94b340043bee526fc9a3762abbc7a3b9ff69e
root <root@ip-172-31-37-70.ap-south-1.compute.internal>
2019-08-21 18:47:10 +0000

    fourth commit

--- gitautomate1.sh
+++ gitautomate1.sh
@@ -6,7 +6,10 @@ echo "enter you file name"
 read file
 echo "your file name is"$file
 git add .
+echo "YOUR FILE IS ADDED"
 echo "enter you commit message"
 read commitmessage
 git commit -m "$commitmessage"
+echo "COMMIT DONE"
 git push
+echo "PUSHED TO GIT HUB"
